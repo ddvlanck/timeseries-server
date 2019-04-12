4afebba4ebd2faf7357c9b31069a87d629b952ff
ddvlanck <Dwight.VanLancker@UGent.be>
2019-04-12 10:08:54 +0200

    everything back to javascripté

--- lib/DataTransformer.ts
+++
@@ -1,6 +0,0 @@
-
-
-class DataTransformer {
-    // This class has a method to transform non-RDF data into RDF data
-    // The output of the method is the RDF data and is passed to the data stream
-}
