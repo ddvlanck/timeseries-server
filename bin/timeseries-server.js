bb8203d847010cb139e1ebed9e6ef1e4528bfda0
ddvlanck <Dwight.VanLancker@UGent.be>
2019-04-12 10:03:35 +0200

    first commit for this branch

--- bin/timeseries-server.js
+++ bin/timeseries-server.js
@@ -1,43 +1,78 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CommunicationManager = require('../lib/CommunicationManager');
-var Configuration = require('../lib/Configuration');
-var DataEventManager = require('../lib/DataEventManager');
-var DataFetcher = require('../lib/DataFetcher');
-var TimeRange = require('../lib/interfaces/TimeRange');
+const CommunicationManager = require('../lib/CommunicationManager');
+const Configuration = require('../lib/Configuration');
+const DataEventManager = require('../lib/DataEventManager');
+const DataMapper = require('../lib/DataMapper');
+
+const kafka = require('kafka-node');
+const Consumer = kafka.Consumer;
+
+const schedule = require('node-schedule');
+
 try {
+
     // Read the configuration file
-    var config = Configuration.getConfig(process.argv);
+    let config = Configuration.getConfig(process.argv);
+
     // Init Communication Manager module
-    var commManager = new CommunicationManager(config);
+    let commManager = new CommunicationManager(config);
+
     // Load multidimensional interfaces
     loadInterfaceModules(config.interfaces, commManager);
-    //////////////////////////////////////////////
-    /*
-    * Getting Air quality data
-    * */
-    var fetcher = new DataFetcher([]);
-    //fetcher.fetchData('');
-    //////////////////////////////////////////////
-    // Listen for data on standard input -- this will be replaced
-    var stdin = process.openStdin();
-    stdin.on('data', function (chunk) {
-        // Launch data event towards interfaces through Data Event Manager module
-        DataEventManager.push('data', chunk);
+
+    // Initialize data mapper
+    let mapper = new DataMapper(
+        (stream) => {
+            stream.dataStream.on('data', (data) => {
+                setTimeout( () => {
+                    DataEventManager.push('data', Buffer.from(data));
+                }, 100);
+
+            });
+        }
+    );
+
+    let sources = new Map();
+
+    // Initiate kafka client
+    let client = new kafka.KafkaClient();    // maybe do something if there's an idle connection
+    let consumer = new Consumer(client, [{topic: 'airquality'}], {fetchMaxBytes: 1024});
+    consumer.on('message', (message) => {
+        const data = JSON.parse(message.value);
+
+        // Check wat die VOC is - blijkt niet meer aanwezig
+        if(data.metricId.indexOf('airquality') >= 0 && data.metricId.indexOf('voc') < 0){
+           // mapper.mapData(data);
+        }
+
     });
+
+    consumer.on('error', (err) => {
+        console.log(err);
+    })
+
+
+    // Listen for data on standard input
+    /*let stdin = process.openStdin();
+     stdin.on('data', chunk => {
+         // Launch data event towards interfaces through Data Event Manager module
+         console.log(chunk.toString());
+        // DataEventManager.push('data', chunk);
+     });*/
+
     // Launch Web server for polling interfaces
-    var app = commManager.app;
-    var router = commManager.router;
+    let app = commManager.app;
+    let router = commManager.router;
     app.use(router.routes()).use(router.allowedMethods());
     app.listen(config.httpPort);
-}
-catch (e) {
+
+} catch (e) {
     console.error(e);
     process.exit(1);
 }
+
 function loadInterfaceModules(interfaces, commManager) {
-    for (var index in interfaces) {
-        var Interface = require(interfaces[index].path);
+    for (let index in interfaces) {
+        let Interface = require(interfaces[index].path);
         new Interface(interfaces[index], commManager);
     }
 }
